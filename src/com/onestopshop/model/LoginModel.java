cb0a3a6ae7869c8f9a96eb5ab9f4bfce7d544966
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2015-10-07 15:50:51 +0530

    Updating hibernate.cfg.xml file. Adding new username and password.

--- src/com/onestopshop/model/LoginModel.java
+++ src/com/onestopshop/model/LoginModel.java
@@ -5,10 +5,10 @@ import com.onestopshop.security.MD5Checksum;
 
 public class LoginModel {
 	
-	public void validateUser(String uname, String password){
+	public boolean validateUser(String uname, String password){
 		password = MD5Checksum.crypt(password);
 		LoginDao objLoginDao = new LoginDao();
-		objLoginDao.checkLogin(uname, password);
+		return objLoginDao.checkLogin(uname, password);
 	}
 
 }
