917f4724ead08d3f1f55a4add0da2bb42d2efa54
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2015-10-07 15:26:51 +0530

    Implementing model and bean for Login module

--- src/com/onestopshop/model/LoginModel.java
+++ src/com/onestopshop/model/LoginModel.java
@@ -1,5 +1,14 @@
 package com.onestopshop.model;
 
+import com.onestopshop.dao.LoginDao;
+import com.onestopshop.security.MD5Checksum;
+
 public class LoginModel {
+	
+	public void validateUser(String uname, String password){
+		password = MD5Checksum.crypt(password);
+		LoginDao objLoginDao = new LoginDao();
+		objLoginDao.checkLogin(uname, password);
+	}
 
 }
