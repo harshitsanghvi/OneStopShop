fa326e6b7b10e6c447e37be665565c4bd4178c04
Harshit Sanghvi <harshit.sanghvi@gmail.com>
2015-10-07 18:05:25 +0530

    Implemented AddressBean

--- src/com/onestopshop/beans/RegisterBean.java
+++ src/com/onestopshop/beans/RegisterBean.java
@@ -25,39 +25,16 @@ public class RegisterBean {
 	private String lastname;
 	
 	@NotEmpty
-	@Column(name = "UNAME", nullable = false)
-	private String uname;
-	
-	@NotEmpty
-	@Column(name = "PASSWORD", nullable = false)
-	private String password;
+	@Column(name = "USERID", nullable = false)
+	private long userId;
 	
 	@NotEmpty
 	@Column(name = "PHONE", nullable = false)
 	private long phone;
 	
 	@NotEmpty
-	@Column(name = "ADDRESSLINE1", nullable = false)
-	private String addressLine1;
-	
-	@Column(name = "ADDRESSLINE2")
-	private String addressLine2;
-	
-	@NotEmpty
-	@Column(name = "CITY", nullable = false)
-	private String city;
-	
-	@NotEmpty
-	@Column(name = "STATE", nullable = false)
-	private String state;
-	
-	@NotEmpty
-	@Column(name = "COUNTRY", nullable = false)
-	private String country;
-	
-	@NotEmpty
-	@Column(name = "ZIP", nullable = false)
-	private int zip;
+	@Column(name = "ADDRESSID", nullable = false)
+	private long addressId;
 	
 	public long getId() {
 		return id;
@@ -77,58 +54,22 @@ public class RegisterBean {
 	public void setLastname(String lastname) {
 		this.lastname = lastname;
 	}
-	public String getUname() {
-		return uname;
-	}
-	public void setUname(String uname) {
-		this.uname = uname;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
 	public long getPhone() {
 		return phone;
 	}
 	public void setPhone(long phone) {
 		this.phone = phone;
 	}
-	public String getAddressLine1() {
-		return addressLine1;
+	public long getUserId() {
+		return userId;
 	}
-	public void setAddressLine1(String addressLine1) {
-		this.addressLine1 = addressLine1;
+	public void setUserId(long userId) {
+		this.userId = userId;
 	}
-	public String getAddressLine2() {
-		return addressLine2;
+	public long getAddressId() {
+		return addressId;
 	}
-	public void setAddressLine2(String addressLine2) {
-		this.addressLine2 = addressLine2;
-	}
-	public String getCity() {
-		return city;
-	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public String getState() {
-		return state;
-	}
-	public void setState(String state) {
-		this.state = state;
-	}
-	public String getCountry() {
-		return country;
-	}
-	public void setCountry(String country) {
-		this.country = country;
-	}
-	public int getZip() {
-		return zip;
-	}
-	public void setZip(int zip) {
-		this.zip = zip;
+	public void setAddressId(long addressId) {
+		this.addressId = addressId;
 	}
 }
